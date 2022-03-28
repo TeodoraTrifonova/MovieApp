7d0ea7e878f27464c5cb698951d8699183990b85
ibu2000 <bezvuzvratno@gmail.com>
2022-03-28 17:12:48 +0300

    gorgeous

--- src/main/java/bg/tu/varna/sit/movieproject/controllers/HomePageController.java
+++ src/main/java/bg/tu/varna/sit/movieproject/controllers/HomePageController.java
@@ -1,9 +1,16 @@
 package bg.tu.varna.sit.movieproject.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class HomePageController {
-
+    @FXML
+    private Button addMovies;
+    @FXML
+    private Button searchMovies;
+    @FXML
+    private Button logout;
 
 
     Stage s =new Stage();
