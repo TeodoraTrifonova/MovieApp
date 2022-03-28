1266a68c60e0d7cee23ea2c32b3febe210ba9d87
ibu2000 <bezvuzvratno@gmail.com>
2022-03-28 16:48:13 +0300

    B-b-b-ba-ba-beautified!

---
+++ src/main/java/bg/tu/varna/sit/movieproject/controllers/HomePageController.java
@@ -0,0 +1,13 @@
+package bg.tu.varna.sit.movieproject.controllers;
+
+import javafx.stage.Stage;
+
+public class HomePageController {
+
+
+
+    Stage s =new Stage();
+    public HomePageController(){ }
+    public HomePageController(Stage stage){s=stage;
+    }
+}
