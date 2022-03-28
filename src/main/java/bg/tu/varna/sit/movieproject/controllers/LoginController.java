35a0e347f846ecdf6a35b3e7c65c45fd92230762
ibu2000 <bezvuzvratno@gmail.com>
2022-03-28 03:30:00 +0300

    *throws butterflies* -Beautified!
    go get gorgeous

--- src/main/java/bg/tu/varna/sit/movieproject/controllers/LoginController.java
+++ src/main/java/bg/tu/varna/sit/movieproject/controllers/LoginController.java
@@ -1,11 +1,31 @@
 package bg.tu.varna.sit.movieproject.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.w3c.dom.Text;
+
+import java.nio.Buffer;
+
 
 public class LoginController {
+
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button login;
+    @FXML
+    private Button register;
+
     Stage s =new Stage();
     public LoginController(){ }
     public LoginController(Stage stage){
         s=stage;
     }
+
+
 }
