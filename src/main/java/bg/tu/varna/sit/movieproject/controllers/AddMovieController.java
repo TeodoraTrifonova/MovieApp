ab6fe618e6986215922849d4a4f855d20f66b684
ibu2000 <bezvuzvratno@gmail.com>
2022-04-02 18:37:18 +0300

    *rides a hornless unicorn* Huzzah!

--- src/main/java/bg/tu/varna/sit/movieproject/controllers/AddMovieController.java
+++ src/main/java/bg/tu/varna/sit/movieproject/controllers/AddMovieController.java
@@ -1,4 +1,27 @@
 package bg.tu.varna.sit.movieproject.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.w3c.dom.Text;
+
 public class AddMovieController {
+
+    @FXML
+    private TextField name;
+    private TextField date;
+    private TextArea actors;
+    private TextArea description;
+    private ComboBox genre;
+    private ComboBox rating;
+    private Button addMovie;
+    private Button goBack;
+
+    Stage s =new Stage();
+    public AddMovieController(){ }
+    public AddMovieController(Stage stage){s=stage;
+    }
 }
